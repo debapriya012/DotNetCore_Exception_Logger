a33d02c18bdd065baa1eef6727496bcd9c4630e1
Depapriya <72795895+Depapriya@users.noreply.github.com>
2021-03-11 13:04:44 +0530

    Add project files.

---
+++ ExceptionHandeling_WithLoggerService/Controllers/StudentController.cs
@@ -0,0 +1,57 @@
+﻿using ExceptionHandeling_WithLoggerService.Models;
+using LoggerService;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ExceptionHandeling_WithLoggerService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : ControllerBase
+    {
+
+        private readonly ILoggerManager _logger;
+
+        public StudentController(ILoggerManager logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            ///Handeling exception Using Try Catch block---------------------------
+            //try
+            //{
+            //    _logger.LogInfo("Fetching all the Students from the storage");
+
+            //    var students = DataManager.GetAllStudents(); //simulation for the data base access
+
+            //    throw new Exception("Exception while fetching all the students from the storage.");
+
+            //    _logger.LogInfo($"Returning {students.Count} students.");
+
+            //    return Ok(students);
+
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    _logger.LogError($"Something went wrong: {ex}");
+            //    return StatusCode(500, "Internal server error");
+            //}
+
+            ///Handeling exception With the help of middleware
+            _logger.LogInfo("Fetching all the Students from the storage");
+
+            var students = DataManager.GetAllStudents(); //simulation for the data base access
+
+            throw new Exception("Exception while fetching all the students from the storage.");
+
+            _logger.LogInfo($"Returning {students.Count} students.");
+
+            return Ok(students);
+
+        }
+    }
+}
---
+++ ExceptionHandeling_WithLoggerService/MiddleWare/CustomExceptionMiddleware.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using ExceptionHandeling_WithLoggerService.Models;
+using LoggerService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace ExceptionHandeling_WithLoggerService.MiddleWare
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class CustomExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILoggerManager _logger;
+
+        public CustomExceptionMiddleware(RequestDelegate next, ILoggerManager logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong: {ex}");
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            return context.Response.WriteAsync(new ErrorDetails()
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "Internal Server Error from the custom middleware."
+            }.ToString());
+        }
+    }
+
+    
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class CustomExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomExceptionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomExceptionMiddleware>();
+        }
+    }
+    
+}
